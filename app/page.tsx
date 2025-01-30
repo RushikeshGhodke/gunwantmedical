245a0f5cf57d989305c86d78ccce4ac7c44e773d
RushikeshGhodke <rushikeshghodke7455@gmail.com>
2025-01-30 22:59:35 +0530

    hero text

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
-import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
+import { useRef } from "react";
+import Navbar from "@/app/components/Navbar";
 import Image from "next/image";
-import {useRef, useState} from "react";
 
 export default function Home() {
-    const [toggleNav, setToggleNav] = useState(false);
     const videoRef = useRef(null);
     return (
         <div className={`bg-[#468585] px-4 py-9 relative w-full h-screen overflow-hidden`}>
+            {/* Video background */}
             <video
                 ref={videoRef}
                 className="absolute inset-0 w-full h-full object-cover"
@@ -16,99 +16,32 @@ export default function Home() {
                 muted
                 playsInline
             >
-                <source src="/video.mp4" type="video/mp4"/>
+                <source src="/video.mp4" type="video/mp4" />
             </video>
+
+            {/* Gradient overlay */}
             <div className="absolute inset-0 bg-gradient-to-r from-[rgba(0,57,57,0.8)] to-[rgba(28,104,104,0.8)] bg-opacity-50"></div>
-            <div className="relative z-10 flex flex-col items-center justify-center h-full px-4 md:px-20 text-white"></div>
-                <header>
-                    <nav
-                        className="fixed instrument-sans top-4 left-4 right-4 z-50 flex items-center justify-between px-6 py-3 bg-white text-[#468585] rounded-2xl lg:px-16 lg:py-4"
-                    >
-                        {/* Logo Section */}
-                        <div className="flex items-center justify-between gap-4">
-                            <Image src={`/Gemini.svg`} alt={`logo`} width={24} height={40}/>
-                            <h1 className="text-[16px] tracking-wide lg:text-[20px]">
-                                Gunwant Medical
-                            </h1>
-                        </div>
 
-                        {/* Hamburger Menu */}
-                        <div className="lg:hidden">
-                            {toggleNav ? (
-                                <XMarkIcon
-                                    onClick={() => setToggleNav(false)}
-                                    className="h-7 w-7 text-[#468585] cursor-pointer hover:text-[#356E6E]"
-                                />
-                            ) : (
-                                <Bars3Icon
-                                    onClick={() => setToggleNav(true)}
-                                    className="h-7 w-7 text-[#468585] cursor-pointer hover:text-[#356E6E]"
-                                />
-                            )}
-                        </div>
+            {/* Navbar */}
+            <Navbar />
 
-                        {/* Desktop Links */}
-                        <div className="hidden lg:flex gap-10">
-                            <a href="#home" className="text-[17px] font-normal hover:text-[#356E6E]">Home</a>
-                            <a href="#gallery" className="text-[17px] font-normal hover:text-[#356E6E]">Gallery</a>
-                            <a href="#categories"
-                               className="text-[17px] font-normal hover:text-[#356E6E]">Categories</a>
-                            <a href="#contact" className="text-[17px] font-normal hover:text-[#356E6E]">Contact Us</a>
-                        </div>
-                    </nav>
+            {/* Hero Section */}
+            <div
+                className="relative mt-7 z-20 flex flex-col gap-4 items-start w-[60%] justify-center h-full px-4 md:px-20 text-[#FFF] stolzl">
+                <h2 className="text-[5.5rem] font-normal text-[#EBEBEB] flex flex-col gap-2 leading-none">
+                    <div>Your <span className="text-[#9CDBA6]">Health</span></div>
+                    <div>Journey,</div>
+                    <div>Made Simple</div>
+                </h2>
 
-                    {/* Expandable Mobile Menu */}
-                    <div
-                        className={`fixed left-4 right-4 top-4 rounded-2xl z-40 h-[90%] bg-white text-[#468585] transform transition-transform duration-300 ${
-                            toggleNav ? "translate-x-0" : "-translate-x-[-120%]"
-                        }`}
-                    >
-                        <ul className="flex flex-col items-center justify-center gap-7 h-full">
-                            <li>
-                                <a
-                                    href="#home"
-                                    className="text-[18px] font-normal hover:text-[#356E6E]"
-                                    onClick={() => setToggleNav(false)}
-                                >
-                                    Home
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#gallery"
-                                    className="text-[18px] font-normal hover:text-[#356E6E]"
-                                    onClick={() => setToggleNav(false)}
-                                >
-                                    Gallery
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#categories"
-                                    className="text-[18px] font-normal hover:text-[#356E6E]"
-                                    onClick={() => setToggleNav(false)}
-                                >
-                                    Categories
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#contact"
-                                    className="text-[18px] font-normal hover:text-[#356E6E]"
-                                    onClick={() => setToggleNav(false)}
-                                >
-                                    Contact Us
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-
-                </header>
-
-                {/* Content below the navbar */}
-                <div className="pt-20">
-                    {/* Add the rest of your content here */}
-                </div>
+                <p className="mt-4 text-[10px] font-light md:text-xl">
+                    Affordable prices. Quick delivery. Trusted care. <br/>Shop everything you need for a healthier
+                    tomorrow.
+                </p>
+                <p className="mt-10 flex lg:text-2xl items-center justify-center md:justify-start font-light gap-2">
+                    ⭐ 4.8/5 rated on <span><Image src={'/google.png'} width={32} height={32} alt={'Google'}/></span>
+                </p>
             </div>
-            );
-            }
+        </div>
+    );
+}
